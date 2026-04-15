56fa7c6edb63836a35580a8455d81eaa7008c76a
jwirjono <jwirjono@gmail.com>
2026-04-16 00:03:51 +0700

    fix handlesubmit

--- src/App.tsx
+++ src/App.tsx
@@ -185,7 +185,7 @@ export default function App() {
     e.preventDefault();
     setIsSubmitting(true);
     try {
-      const response = await fetch("http://localhost:3000/api/contact", {
+      const response = await fetch("api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formState),
